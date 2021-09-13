cd6ec89126cb441db76b729a59bfb820774b6a69
Alla <alla.nest.1@yandex.ru>
2021-09-13 10:41:38 +0300

    create

--- send.php
+++ send.php
@@ -8,7 +8,7 @@ if(isset($_POST['f_e_mailv']) && isset($_POST['f_phonev'])){
     $theme = "=?utf-8?B?".base64_encode("Заявка Орлова")."?=";
     $headers = "From: \r\nContent-type: text/html; charset=utf-8\r\n";
 
-    $success = mail("alla.nest.1@yandex.ru", $theme, $f_e_mailv ,$f_phonev, $headers);
+    $success = mail("order@salesgenerator.pro", $theme, $f_e_mailv ,$f_phonev, $headers);
     echo $success;
 
     
